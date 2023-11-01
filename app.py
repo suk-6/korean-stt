27235d096a33de5d699e8446eb1eabe5e32f49a5
suk-6 <me@suk.kr>
2023-11-01 14:08:16 +0900

    Feat: Add Now Speaking

--- app.py
+++ app.py
@@ -21,11 +21,20 @@ class app:
         self.defaultPageTitle = tk.Label(
             self.defaultPage,
             text="일제강점기 잔재 언어 탐지기",
-            font=("Arial", 40),
+            font=("Arial", 60),
             fg="black",
             bg="white",
         )
         self.defaultPageTitle.pack()
+
+        self.nowSpeaking = tk.Label(
+            self.defaultPage,
+            text="",
+            font=("Arial", 30),
+            fg="black",
+            bg="white",
+        )
+        self.nowSpeaking.pack()
 
         self.window.after(1000, self.sttStart)
 
@@ -45,21 +54,20 @@ class app:
         mySpeech = r.recognize_google(audio, language='ko', show_all=True)
         try:
             if mySpeech == []:
-                return 0
+                return
             
             strData = mySpeech['alternative'][0]['transcript']
             print(strData)
+            self.nowSpeaking.configure(text=strData)
 
             for banWord in wordDict.keys():
                 if banWord in strData:
                     print(f"{banWord} -> {wordDict[banWord]}")
                     self.defaultPageTitle.configure(text=f"{banWord} -> {wordDict[banWord]}")
-                    self.window.update()
-                    return 1
 
-            return 0
+            self.window.update()
         except:
-            return 0
+            return
 
     def toggleFullScreen(self, event):
         self.fullScreenState = not self.fullScreenState
